d7fa20570e7a26b6d994bf645292d6b2c29f383c
mhornkjol <martin.hornkjol@hotmail.com>
2024-03-12 11:22:27 +0100

    Added code and stl files needed for chapter 3.

---
+++ create_mesh.py
@@ -0,0 +1,68 @@
+import SVMTK as svm
+from pathlib import Path
+
+def make_choroid_plexus(lh_choroid_plexus, rh_choroid_plexus):
+    lh_choroid_plexus.keep_largest_connected_component()
+    rh_choroid_plexus.keep_largest_connected_component()
+    lh_choroid_plexus.fill_holes()
+    rh_choroid_plexus.fill_holes()
+
+    lh_choroid_plexus.union(rh_choroid_plexus)
+    choroid_plexus= svm.Surface(lh_choroid_plexus)
+    choroid_plexus.fill_holes()
+    choroid_plexus.repair_self_intersections()
+
+    return choroid_plexus
+
+
+
+if __name__=='__main__':
+    print("Start ",__file__)
+    outdir = Path('mesh')
+    outdir.mkdir(exist_ok=True)
+
+    white = svm.Surface("stl_files/white.final.stl")
+    lh_pial = svm.Surface("stl_files/lhpial.final.stl")
+    rh_pial = svm.Surface("stl_files/rhpial.final.stl")
+    bounding_surface = svm.Surface("stl_files/dura.final.stl")
+    ventricles = svm.Surface("stl_files/ventricles.final.stl")
+    lh_choroid_plexus = svm.Surface("stl_files/lhchoroid.plexus.final.stl")
+    rh_choroid_plexus = svm.Surface("stl_files/rhchoroid.plexus.final.stl")
+    choroid_plexus = make_choroid_plexus(lh_choroid_plexus, rh_choroid_plexus)
+    cisterna_magna = svm.Surface("stl_files/cisterna.magna.stl")
+
+    ventricles.enclose(choroid_plexus)
+    choroid_plexus.embed(ventricles)
+    ventricles.separate(choroid_plexus)
+
+    white.enclose(ventricles)
+    ventricles.embed(white)
+    white.separate(ventricles)
+
+    white.difference(cisterna_magna)
+
+    bounding_surface.adjust_boundary(2)
+
+    p1 = svm.Point_3(-4, -23, 3.7)
+    p2 = svm.Point_3(-4, -30, -3.7)
+    aqueduct = svm.Surface(ventricles)
+    aqueduct.clip(p1,svm.Vector_3(p1,p2), 4.)
+    aqueduct.clip(p2,svm.Vector_3(p2,p1), 4.)
+
+    surfaces = [bounding_surface, lh_pial, rh_pial, white, ventricles, aqueduct, choroid_plexus]
+
+    smap = svm.SubdomainMap(7)
+    smap.add("1000000", 1)
+    smap.add("*100000", 2)
+    smap.add("*010000", 2)
+    smap.add("*110000", 2)
+    smap.add("*1000", 3)
+    smap.add("*100", 4)
+    smap.add("*10", 5)
+    smap.add("*1", 6)
+
+    domain = svm.Domain(surfaces, smap)
+    domain.create_mesh(32)
+    domain.save(str(outdir/"brain_mesh.mesh"))
+
+    print("Finish ",__file__)
---
+++ stokes.py
@@ -0,0 +1,72 @@
+from fenics import *
+
+mu = 8e-4
+
+parameters["krylov_solver"]["monitor_convergence"] = True
+
+mesh = Mesh()
+hdf = HDF5File(mesh.mpi_comm(), "mesh/brain_mesh.h5", "r")
+hdf.read(mesh, "/mesh", False)
+subdomains = MeshFunction("size_t", mesh, mesh.topology().dim())
+hdf.read(subdomains, "/subdomains")
+boundaries  = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
+hdf.read(boundaries, "/boundaries")
+
+P2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
+P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+TH = P2 * P1
+W = FunctionSpace(mesh, TH)
+
+(u, p) = TrialFunctions(W)
+(v, q) = TestFunctions(W)
+
+n = FacetNormal(mesh)
+
+class BoundaryAG(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] > -31 and x[0] < 18 and x[1] > -65 \
+            and x[1] < 13 and x[2] > 60 and on_boundary
+
+Outflow = BoundaryAG(); Outflow.mark(boundaries, 16)
+
+no_slip = Constant((0.0, 0.0, 0.0))
+bc1 = DirichletBC(W.sub(0), no_slip, boundaries, 7)
+bc2 = DirichletBC(W.sub(0), no_slip, boundaries, 8)
+bc3 = DirichletBC(W.sub(0), no_slip, boundaries, 9)
+bc4 = DirichletBC(W.sub(0), no_slip, boundaries, 12)
+bc5 = DirichletBC(W.sub(0), no_slip, boundaries, 13)
+
+bcs = [bc1, bc2, bc3, bc4, bc5]
+
+dx = Measure("dx", domain=mesh, subdomain_data=subdomains)
+dxP = Measure('dx', domain=mesh, subdomain_data=subdomains, \
+              subdomain_id=(2,3))
+dxF = Measure('dx', domain=mesh, subdomain_data=subdomains, \
+              subdomain_id=(1,4,5,6))
+ds = Measure("ds", domain=mesh, subdomain_data=boundaries)
+dS = Measure("dS", domain=mesh, subdomain_data=boundaries)
+
+g_source = Constant(0.006896552)
+
+a = mu*inner(grad(u), grad(v))*dxF - div(v)*p*dxF - q*div(u)*dxF
+p = mu*inner(grad(u), grad(v))*dxF + (1.0/mu)*p*q*dxF
+
+L = - g_source*q*dx(6)
+
+A, b = assemble_system(a, L, bcs)
+P, _ = assemble_system(p, L, bcs)
+A.ident_zeros()
+P.ident_zeros()
+
+U = Function(W)
+solver = KrylovSolver("minres", "amg")
+solver.set_operators(A, P)
+it = solver.solve(U.vector(), b)
+u, p = U.split(deepcopy=True)
+print(f'it {it}')
+
+with XDMFFile(MPI.comm_world, 'solution/velocity.xdmf') as xdmf:
+    xdmf.write_checkpoint(u, "velocity", 0)
+
+with XDMFFile(MPI.comm_world, 'solution/pressure.xdmf') as xdmf:
+    xdmf.write_checkpoint(p, "pressure", 0)
